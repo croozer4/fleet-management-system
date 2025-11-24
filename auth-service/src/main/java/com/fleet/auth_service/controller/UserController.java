1d5a1197ceaeafae77f824abfbf26e676f7a508b
croozer4 <konradkul12@gmail.com>
2025-11-25 00:46:05 +0100

    Password encoding for new User

--- auth-service/src/main/java/com/fleet/auth_service/controller/UserController.java
+++ auth-service/src/main/java/com/fleet/auth_service/controller/UserController.java
@@ -2,6 +2,7 @@ package com.fleet.auth_service.controller;
 
 import com.fleet.auth_service.model.User;
 import com.fleet.auth_service.repository.UserRepository;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
 import  java.util.List;
@@ -9,8 +10,10 @@ import  java.util.List;
 @RequestMapping("/api/users")
 public class UserController {
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
-    public UserController(UserRepository userRepository) {
+    public UserController(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+        this.passwordEncoder = passwordEncoder;
         this.userRepository = userRepository;
     }
 
@@ -21,6 +24,7 @@ public class UserController {
 
     @PostMapping
     public User createUser(@RequestBody User user){
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
         return userRepository.save(user);
     }
 }
